9fd4f515c813e85746bd5dc189878f91022fe892
gabi <133364342+gabicerqueira@users.noreply.github.com>
2024-05-09 15:45:44 -0300

    Splash

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await new Promise(resolve => setTimeout(resolve, 5000));
+        await SplashScreen.hideAsync();
+      } catch (erro) {
+        console.error(erro);
+      } 
+    }
+    prepare();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
